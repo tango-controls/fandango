85baee32ecbdd7c486863f85b911ab921be974e4
sergi_rubio <sergi_rubio@4e9c00fd-8f2e-0410-aa12-93ce3db5e235>
2016-04-12 16:16:13 +0000

    tango:sorted methods, added device launchers

--- share/fandango/trunk/setup.py
+++ share/fandango/trunk/setup.py
@@ -1,12 +1,37 @@
+#!/usr/bin/env python
+# Always prefer setuptools over distutils
 import os, imp
-
 from setuptools import setup, find_packages
+
+__doc__ = """
+To install as system package:
+
+  python setup.py install
+  
+To install as local package:
+
+  RU=/opt/control
+  python setup.py egg_info --egg-base=tmp install --root=$RU/files --no-compile \
+    --install-lib=lib/python/site-packages --install-scripts=ds
+
+-------------------------------------------------------------------------------
+"""
+
+print(__doc__)
 
 ReleaseNumber = type('ReleaseNumber',(tuple,),
                      {'__repr__':(lambda self:'.'.join(map(str,self)))})
 release = ReleaseNumber(
     imp.load_source('changelog',os.path.join('.', 'fandango', 'CHANGES'))
     .RELEASE)
+
+scripts = [
+'./fandango/scripts/fandango.sh',
+'./fandango/scripts/fandango.qt',
+'./fandango/scripts/DynamicDS',
+'./fandango/scripts/tango_servers',
+]
+
 
 setup(
     name="fandango",
@@ -38,5 +63,6 @@ setup(
     ],
     platforms=[ "Linux,Windows XP/Vista/7/8" ],
     install_requires=[],
+    scripts=scripts,
     include_package_data=True,
     zip_safe=False)
