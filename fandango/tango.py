490d33eac6752b648eea8856ae60596a1d0ae655
srubio <srubio@pt143.cells.es>
2016-10-08 18:39:25 +0200

    solved bug on fakeAttributeValue initialization

--- fandango/tango.py
+++ fandango/tango.py
@@ -1372,7 +1372,7 @@ class fakeAttributeValue(object):
     """
     def __init__(self,name,value=None,time_=0.,quality=PyTango.AttrQuality.ATTR_VALID,dim_x=1,dim_y=1,parent=None,device='',error=False,keeptime=0):
         self.name=name
-        self.device=device or (self.name.split('/')[-1] if '/' in self.name else '')
+        self.device=device or (self.name.rsplit('/',1)[0] if '/' in self.name else '')
         self.set_value(value,dim_x,dim_y)
         self.set_date(time_ or time.time())
         self.write_value = None
