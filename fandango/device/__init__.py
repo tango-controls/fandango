85facdd9d16ffe941b2c32856046723ace0749a7
Sergio Rubio Manrique <srubio@cells.es>
2016-10-19 12:37:04 +0200

    Solve FolderAPI import errors

--- fandango/device/__init__.py
+++ fandango/device/__init__.py
@@ -1,13 +1,21 @@
 
 try:
+  import DDebug as debug
   from DDebug import *
 except:
   print('Unable to import DDebug')
 try:
+  import Dev4Tango as dev4tango
   from Dev4Tango import *
 except:
   print('Unable to import Dev4Tango')
 try:
-  from FolderDS import FolderDS, FolderDSClass, FolderAPI
+  import FolderDS as folders
+  from FolderDS import FolderDS,FolderAPI
 except:
   print('Unable to import FolderDS')
+try:
+  import WorkerDS as workers
+except:
+  print('Unable to import WorkerDS')
+
