2843367b65442208af54b3b8874efa94f6cc1f2e
Sergio Rubio Manrique <srubio@cells.es>
2017-03-16 09:54:54 +0100

    Update VERSION to 12.6.1

--- fandango/__init__.py
+++ fandango/__init__.py
@@ -57,7 +57,7 @@ try:
     import pkg_resources
     __version__ = pkg_resources.get_distribution(__name__).version
 except Exception, e:
-    __version__ = None
+    __version__ = RELEASE
     #print ('Unable to get distribution version number, fandango has '
     #       'probably not been installed as a package')
     
